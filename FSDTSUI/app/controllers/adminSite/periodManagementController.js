af2897782ed98088cd8c91d2f5f04b78ad38a5f1
Ashwini.Bhalerao <Ashwini.Bhalerao@Extentia.com>
2014-11-19 18:46:41 +0530

    - refactoring period management controller

--- FSDTSUI/app/controllers/adminSite/periodManagementController.js
+++ FSDTSUI/app/controllers/adminSite/periodManagementController.js
@@ -10,6 +10,7 @@ fsdtsApp.controller('periodManagementController', ['$scope', '$routeParams', 'ap
      $scope.value = " ";
      $scope.periodList = [];
      $scope.selectedPeriod = null;
+     $scope.periodInfo = {};
 
      //Submit button click handler
      $scope.onSubmit = function (event) {
@@ -129,7 +130,7 @@ fsdtsApp.controller('periodManagementController', ['$scope', '$routeParams', 'ap
          //#region initialize scope variables
          $scope.yearList = getYearList(userProfileService.profile.params.startYear, userProfileService.profile.params.endYear);
          $scope.actionType = $routeParams.actionType;
-         $scope.periodInfo = { 'year': new Date().getFullYear().toString() };
+         $scope.periodInfo = { 'year': userProfileService.profile.params.startYear };
          $scope.confirmWindowOption = {
              actionType: null,
              showConfirm: false
