82bab9016e69c3c792f4eddbd70af3519b9671cd
Ashwini.Bhalerao <Ashwini.Bhalerao@Extentia.com>
2014-11-21 19:04:33 +0530

    - implementing maintanance functionality

---
+++ FSDTSUI/app/controllers/adminSite/maintenanceManagementController.js
@@ -0,0 +1,160 @@
+﻿/**
+Is used to provide all event handling logic for user management view i.e periodManagement.html
+
+*/
+'use strict';
+fsdtsApp.controller('maintenanceManagementController', ['$scope', '$routeParams', 'appConstants', 'maintenanceManagementService', '$location', 'userProfileService',
+ function ($scope, $routeParams, appConstants, maintenanceManagementService, $location, userProfileService) {
+
+     //TODO: Optimization : Dummy implementation need to be removed
+     $scope.maintenanceList = [];
+     $scope.selectedMaintenance = null;
+     $scope.maintenanceInfo = {};
+     $scope.showAddButton = true;
+
+     //Submit button click handler
+     $scope.onSubmit = function (event) {
+         event.preventDefault();
+         if ($scope.validator.validate()) {  // code for validation
+             $scope.validationClass = "valid";
+             $scope.confirmWindowOption.actionType = "Submit";
+             $scope.confirmWindowOption.showConfirm = true;
+         } else {
+             $scope.validationClass = "invalid";
+         }
+     };
+
+     //Delete button click handler
+     $scope.onDelete = function (maintenance) {
+         $scope.selectedMaintenance = maintenance;
+         $scope.confirmWindowOption.actionType = "Delete";
+         $scope.confirmWindowOption.showConfirm = true;
+     };
+
+     //call back handler for confirmation window
+     $scope.confirmActionHandler = function (actionType, isConfirmed) {
+         if ((actionType === 'Submit') && (isConfirmed === true)) {
+             //code for submit
+             if ($scope.showAddButton === true) { //Add maintenance
+                 addMaintenance();
+             } else {//Edit maintenance
+                 editMaintenance();
+                 $scope.showAddButton = true;
+             }
+         } else if ((actionType === 'Delete') && (isConfirmed === true)) { //code for delete
+             deleteMaintenance($scope.selectedMaintenance);
+         } else if ((actionType === 'Cancel') && (isConfirmed === true)) { //code for cancel
+             resetForm();
+             $scope.showAddButton = true;
+        }
+         //Clear selected Row
+         $scope.selectedMaintenance = null;
+     };
+
+     $scope.onActionClick = function (maintenance) {
+         $scope.showAddButton = false;
+         userProfileService.profile.params.commonProgramsGroupingId = maintenance.commonProgramsGroupingId; ;
+         $scope.maintenanceInfo = {
+             commonProgramGroupings: maintenance.commonProgramGroupings
+         };
+     };
+
+     //On cancel button click handler
+     $scope.onCancel = function () {
+         $scope.confirmWindowOption.actionType = "Cancel";
+         $scope.confirmWindowOption.showConfirm = true;
+     };
+
+     //Showing error window
+     var showErrorWindow = function (errorMessage) {
+         $scope.errorWindowOption.showError = true;
+         $scope.errorWindowOption.errorMessage = errorMessage;
+         $scope.showSpin = false;
+     };
+
+     //Reset the form control
+     var resetForm = function () {
+         $scope.maintenanceInfo = {
+             commonProgramGroupings: ' '
+         };
+         $scope.maintenanceList = [];
+         getAllMaintenances();
+     };
+
+     //Service call to edit maintenance
+     var editMaintenance = function () {
+         //Show spin window
+         $scope.showSpin = true;
+         maintenanceManagementService.editMaintenance($scope.maintenanceInfo).then(function () {
+             //Show spin window
+             $scope.showSpin = false;
+             //After updating  maintenance redirect to course management page
+             resetForm();
+         }, function (error) {
+             showErrorWindow(error);
+         });
+     };
+
+     //Service call to add maintenance
+     var addMaintenance = function () {
+         //Show spin window
+         $scope.showSpin = true;
+         maintenanceManagementService.addMaintenance($scope.maintenanceInfo).then(function (result) {
+             //Hide spin window
+             $scope.showSpin = false;
+             //After adding maintenance redirect to course management page
+             resetForm();
+         }, function (error) {
+             showErrorWindow(error);
+         });
+     };
+
+     //Service call to delete maintenance
+     var deleteMaintenance = function (selectedMaintenance) {
+         //Show spin window
+         $scope.showSpin = true;
+         maintenanceManagementService.deleteMaintenance(selectedMaintenance).then(function (result) {
+             console.log(result);
+             //Hide spin window
+             $scope.showSpin = false;
+             //After adding course redirect to period management page
+             resetForm();
+         }, function (error) {
+             showErrorWindow(error);
+         });
+     };
+
+     var populateMaintenanceList = function (maintenanceList) {
+         angular.forEach(maintenanceList, function (maintenance) {
+             $scope.maintenanceList.push(maintenanceManagementService.populateMaintenanceModel(maintenance));
+         });
+     };
+
+     //Get all periods
+     var getAllMaintenances = function () {
+         maintenanceManagementService.getMaintenanceDetails().then(function (result) {
+             console.log(result);
+             populateMaintenanceList(result);
+         }, function (error) {
+             showErrorWindow(error);
+         });
+     };
+
+     //used for initializing the controller
+     var init = function () {
+         //#region initialize scope variables
+         // $scope.yearList = getYearList(userProfileService.profile.params.startYear, userProfileService.profile.params.endYear);
+         $scope.actionType = $routeParams.actionType;
+         $scope.confirmWindowOption = {
+             actionType: null,
+             showConfirm: false
+         };
+         $scope.errorWindowOption = {
+             showError: false,
+             errorMessage: null
+         };
+         //#endregion
+         getAllMaintenances();
+     } ();
+
+ } ]); 
---
+++ FSDTSUI/app/services/adminSite/maintenanceManagementService.js
@@ -0,0 +1,122 @@
+﻿/**  Service descriptions
+/**  Service descriptions
+*Used to provide all the functionality required for maintenance module
+    
+*Expose method :
+addMaintenance(maintenanceInfo)
+post data format :
+var postData = {
+    "CommonProgramsGroupingId": 1,
+    "CommonProgramsGroupingName": "sample string 2",
+    "CommonProgramsGroupingLastEditedOn": "2014-11-21T13:21:19.7120765+05:30",
+    "CommonProgramsGroupingLastEditedBy": "sample string 4",
+    "IsDeleted": true
+};
+
+editMaintenance(maintenanceInfo)
+getMaintenanceDetails(commonProgramsGroupingId)
+*/
+
+'use strict';
+fsdtsApp.factory('maintenanceManagementService', ['httpHelper', 'appConstants', 'userProfileService',
+    function (httpHelper, appConstants, userProfileService) {
+        var serviceInstance = {};
+
+        /** Adding new maintenance 
+        * Method:   addMaintenance
+        * Access:   Public 
+        * @param    maintenanceInfo object
+        * @return   promise
+        */
+        serviceInstance.addMaintenance = function (maintenanceInfo) {
+            var postData = getPostData(maintenanceInfo, appConstants.OPERATION_TYPE.ADD);
+            return httpHelper.post(appConstants.API_END_POINTS.MAINTAINANCE, postData);
+        };
+
+        /** Edit existing Maintenance 
+        * Method:   editMaintenance
+        * Access:   Public 
+        * @param    courseInfo object
+        * @return   promise
+        */
+        serviceInstance.editMaintenance = function (maintenanceInfo) {
+            var postData = getPostData(maintenanceInfo, appConstants.OPERATION_TYPE.EDIT);
+            return httpHelper.put(appConstants.API_END_POINTS.MAINTAINANCE + userProfileService.profile.params.commonProgramsGroupingId, postData);
+        };
+
+
+        /** USed to delete(soft) the selected Maintenance
+        * Method:   deleteMaintenance
+        * Access:   Public 
+        * @return   promise
+        */
+        serviceInstance.deleteMaintenance = function (maintenanceInfo) {
+            var postData = getPostData(maintenanceInfo, appConstants.OPERATION_TYPE.DELETE);
+            return httpHelper.patch(appConstants.API_END_POINTS.MAINTAINANCE + maintenanceInfo.commonProgramsGroupingId, postData);
+        };
+
+        /** Return Maintenance details by commonProgramsGroupingId
+        * Method:   getMaintenanceDetails
+        * Access:   Public 
+        * @param    maintenanceId
+        * @return   promise
+        */
+        serviceInstance.getMaintenanceDetails = function () {
+            return httpHelper.get(appConstants.API_END_POINTS.MAINTAINANCE);
+        };
+
+        /** Return client  maintenance data model by mapping to the server data model
+        * Method:   populateMaintenanceModel
+        * Access:   Public 
+        * @param    Maintenance details server  response
+        * @return   maintenanceInfo object
+        */
+        serviceInstance.populateMaintenanceModel = function (serverResponseObj) {
+            var maintenanceInfo = {};
+            if (serverResponseObj) {
+                maintenanceInfo = {
+                    'commonProgramsGroupingId': serverResponseObj.CommonProgramsGroupingId, //If commonProgramsGroupingId is null or undefine then initialized with empty string
+                    'commonProgramGroupings': serverResponseObj.CommonProgramsGroupingName
+                };
+            }
+            return maintenanceInfo;
+        };
+
+        /** Create the post data required by service for add/edit/get maintenance 
+        * Method:   getPostData
+        * Access:   Private 
+        * @param    maintenanceInfo object
+        * @return   postData object
+        */
+        var getPostData = function (maintenanceInfo, actionType) {
+            var postData = null;
+            try {
+                if (actionType === appConstants.OPERATION_TYPE.ADD) {
+                    postData = {
+                        'CommonProgramsGroupingName': maintenanceInfo.commonProgramGroupings,
+                        'CommonProgramsGroupingLastEditedOn': '2014-11-21T13:21:19.7120765+05:30',
+                        'CommonProgramsGroupingLastEditedBy': 'sample string 4',
+                        'IsDeleted': false
+                    };
+                } else if (actionType === appConstants.OPERATION_TYPE.DELETE) {
+                    postData = {
+                        'IsDeleted': true
+                    };
+                } else if (actionType === appConstants.OPERATION_TYPE.EDIT) {
+                    postData = {
+                        'CommonProgramsGroupingName': maintenanceInfo.commonProgramGroupings,
+                        'CommonProgramsGroupingLastEditedOn': '2014-11-21T13:21:19.7120765+05:30',
+                        'CommonProgramsGroupingLastEditedBy': 'sample string 4',
+                        'IsDeleted': false,
+                        'CommonProgramsGroupingId': userProfileService.profile.params.commonProgramsGroupingId
+                    }
+                }
+                console.log();
+            } catch (e) {
+                console.log('Error on creating postData', e);
+            }
+            return postData;
+        };
+        return serviceInstance;
+    }
+]);
